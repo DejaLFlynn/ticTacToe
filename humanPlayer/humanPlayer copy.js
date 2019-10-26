da2c046757f3183227511779193424457ba13251
DejaLFlynn <52140189+DejaLFlynn@users.noreply.github.com>
2019-10-26 10:07:08 -0400

    Add files via upload

---
+++ humanPlayer/humanPlayer copy.js
@@ -0,0 +1,12 @@
+const readlineSync = require('readline-sync');
+class HumanPlayer{
+    constructor(name, sym){
+        this.name = name;
+        this.sym = sym
+    }
+    getMove(move){
+        return 
+    }
+}
+let game = HumanPlayer(getMove())
+console.log(game)
